434f510ceb9f3ba531fcdf472eda6528f7835e88
Beck LaBash <55890162+becklabs@users.noreply.github.com>
2023-04-11 01:13:51 -0400

    Build uncontaminated leetcode dataset

--- leetcode_env/utils.py
+++ leetcode_env/utils.py
@@ -40,6 +40,25 @@ def id_from_slug(slug: str, api_instance) -> str:
     frontend_id = response['data']['question']['question_id']
     return frontend_id
 
+def metadata_from_slug(slug: str, api_instance) -> str:
+    """
+    Retrieves the metadata of the question with the given slug
+    """
+    graphql_request = leetcode.GraphqlQuery(
+      query="""
+                  query getQuestionDetail($titleSlug: String!) {
+                    question(titleSlug: $titleSlug) {
+                      metaData
+                    }
+                  }
+              """,
+              variables={"titleSlug": slug},
+              operation_name="getQuestionDetail",
+    )
+    response = ast.literal_eval(str(api_instance.graphql_post(body=graphql_request)))
+    metadata = response['data']['question']
+    return metadata
+
 
 class SubmissionFormatter(ABC):
     """
